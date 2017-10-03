c15c20ffe34f36ec12972856c6e2eb2cc7876bc4
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-10-03 19:21:56 -0400

    Add project files.

---
+++ Assignment 3/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        Me.lstResult = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Location = New System.Drawing.Point(96, 254)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(75, 23)
+        Me.btnDraw.TabIndex = 0
+        Me.btnDraw.Text = "Draw"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'lstResult
+        '
+        Me.lstResult.FormattingEnabled = True
+        Me.lstResult.Location = New System.Drawing.Point(13, 13)
+        Me.lstResult.Name = "lstResult"
+        Me.lstResult.Size = New System.Drawing.Size(239, 186)
+        Me.lstResult.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 222)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter number:"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(87, 219)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(122, 20)
+        Me.txtInput.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 299)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstResult)
+        Me.Controls.Add(Me.btnDraw)
+        Me.Name = "Form1"
+        Me.Text = "Assignment 3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDraw As Button
+    Friend WithEvents lstResult As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+End Class
---
+++ Assignment 3/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+
+        Dim n As Integer = txtInput.Text
+        Dim print As String = ""
+
+        For i = 1 To n
+            For j = 1 To n - i
+                print = print & "#"
+            Next
+            print = print & "&" & ControlChars.CrLf
+
+            For j = 1 To (i - 1) * 2
+                print = print & "%"
+            Next
+            print = print & "*" & ControlChars.CrLf
+            lstResult.Items.Add(print)
+            print = ""
+        Next
+
+    End Sub
+End Class
